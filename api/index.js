58eb5220a20f0b1307f198fe7adf616153cf6a80
Basim Hilal V <basimhilalofficial@gmail.com>
2023-11-25 10:02:34 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log('DB Connected');
+}).catch((err)=>{
+    console.log(err);
+})
+
 const app = express();
 
 app.listen(3000, ()=>{
