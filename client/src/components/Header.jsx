0ff789358281b79f606080635f99338fb6e62b81
Basim Hilal V <basimhilalofficial@gmail.com>
2023-11-25 14:51:22 +0530

    signup page UI changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
+import { IoSearchOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="bg-blue-900 shadow-md">
+    <header className="bg-blue-900 shadow-md bg-opacity-50">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-gray-500">basi.</span>
-            <span className="text-gray-300">space</span>
+            <span className="text-blue-400">basi.</span>
+            <span className="text-blue-200">space</span>
           </h1>
         </Link>
-        <form className="bg-blue-800 p-2 rounded-lg flex items-center">
+        <form className="bg-blue-800 bg-opacity-50 p-2 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search.."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="bg-transparent pl-1 focus:outline-none text-blue-200 w-24 sm:w-64"
           />
-          <FaSearch className="bg-blue-800" />
+          <IoSearchOutline className="bg-transparent" />
         </form>
         <ul className="flex gap-3">
             <Link to='/'>
-          <li className="hidden sm:inline text-gray-400 hover:text-gray-300">
+          <li className="hidden sm:inline text-blue-300 hover:text-blue-200">
             Home
           </li>
           </Link>
           <Link to='/about'>
-          <li className="hidden sm:inline text-gray-400 hover:text-gray-300">
+          <li className="hidden sm:inline text-blue-300 hover:text-blue-200">
             About
           </li>
           </Link>
           <Link to='/sign-in'>
-          <li className="text-gray-400 hover:text-gray-300">Sign in</li>
+          <li className="text-blue-300 hover:text-blue-200">Sign in</li>
           </Link>
         </ul>
       </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,46 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+    <div className="p-3 max-w-lg mx-auto bg-blue-900 my-24 bg-opacity-50 shadow-md rounded-xl">
+      <h1 className="text-3xl text-center text-blue-200 font-bold my-5">
+        Sign Up
+      </h1>
+      <form className="flex flex-col gap-5 mt-7">
         <input
           type="text"
           placeholder="Username"
-          className="border p-3 rounded-lg"
+          className="border-none  bg-blue-100 bg-opacity-25 focus:outline-none focus:placeholder-transparent text-blue-200 p-3 rounded-lg w-80 m-auto"
           id="usernmae"
         />
         <input
           type="email"
           placeholder="Email"
-          className="border p-3 rounded-lg"
+          className="border-none bg-blue-100 bg-opacity-25 focus:outline-none focus:placeholder-transparent text-blue-200 p-3 rounded-lg w-80 m-auto"
           id="email"
         />
         <input
           type="text"
           placeholder="Password"
-          className="border p-3 rounded-lg"
+          className="border-none bg-blue-100 bg-opacity-25 focus:outline-none focus:placeholder-transparent text-blue-200 p-3 rounded-lg w-80 m-auto"
           id="password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+        <button
+          className="bg-blue-700 bg-opacity-70 mb-7 mt-3
+         text-blue-200 w-80 m-auto p-3 
+         rounded-lg shadow-sm uppercase hover:opacity-90 disabled:opacity-30"
+        >
           Sign up
         </button>
       </form>
-      <div className="flex gap-2 mt-5">
+      
+      {/* <div className="flex gap-2 text-blue-100 text-center">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign in</span>
+          <span className="text-blue-300 text-center">Sign in</span>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
