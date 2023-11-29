9f98e4a9e62dc297be4990445176591197d0fbbe
Basim Hilal V <basimhilalofficial@gmail.com>
2023-11-29 09:13:40 +0530

    created listing api route

---
+++ api/models/listing.model.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const listingSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    created:{
+        type:String,
+        required:true,
+    },
+    seller:{
+        type:String,
+        required:true,
+    },
+    regularPrice:{
+        type:Number,
+        required:true
+    },
+    discountPrice:{
+        type:Number,
+        required:true
+    },
+    compatibility:{
+        type:String,
+        required:true,
+    },
+    offer:{
+        type:String,
+        required:true,
+    },
+    imageUrls:{
+        type:Array,
+        required:true,
+    },
+    userRef:{
+        type:String,
+        required:true
+    }
+
+},{
+    timestamps:true
+})
+
+const Listing = mongoose.model('Listing', listingSchema);
+export default Listing;
