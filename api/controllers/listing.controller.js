ad3e2ffb9c6e5d6c4f8d75dadff635cfc1a767db
Basim Hilal V <basimhilalofficial@gmail.com>
2023-12-02 20:15:33 +0530

    get all listing route api configured for search

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,55 +1,105 @@
-import Listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const createListing = async (req,res,next) =>{
-    try {
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-    } catch (error) {
-        next(error)
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(400, "Not authorized to delete this listing"));
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Deleted Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(400, "Not authorized to update this listing"));
+  }
+  try {
+    const updateList = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(201).json(updateList);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) => {
+  try {
+    const list = await Listing.findById(req.params.id);
+    if (!list) {
+      return next(errorHandler(404, "No List found"));
     }
+    res.status(200).json(list);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllListing = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let rent = req.query.rent;
+    if (rent === undefined || rent === "false") {
+      rent = { $in: [false, true] };
+    }
+
+    let sale = req.query.sale;
+    if (sale === undefined || sale === "false") {
+      sale = { $in: [false, true] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const list = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      rent,
+      sale,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+    return res.status(200).json(list);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const test = async (req,res,next)=>{
+    const list = await Listing.find();
+    res.json(list)
 }
 
-export const deleteListing = async (req,res,next) =>{
-    const listing = await Listing.findById(req.params.id)
-    if(!listing){
-        return next(errorHandler(404, "Not found"));
-    }
-    if(req.user.id !== listing.userRef){
-        return next(errorHandler(400, "Not authorized to delete this listing"));
-    }
-    try {
-        await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json('Deleted Successfully');
-    } catch (error) {
-        next(error)
-    }
-}
 
-export const updateListing = async (req,res,next) =>{
-    const listing = await Listing.findById(req.params.id)
-    if(!listing){
-        return next(errorHandler(404, "Not found"));
-    }
-    if(req.user.id !== listing.userRef){
-        return next(errorHandler(400, "Not authorized to update this listing"));
-    }
-    try {
-        const updateList = await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        res.status(201).json(updateList);
-    } catch (error) {
-        next(error)
-    }
-}
-
-export const getListing = async (req,res,next)=>{
-    try {
-        const list = await Listing.findById(req.params.id);
-        if(!list){
-            return next(errorHandler(404,"No List found"))
-        }
-        res.status(200).json(list);
-    } catch (error) {
-        next(error);
-    }
-}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getAllListing, test } from '../controllers/listing.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 const router =express.Router();
 
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
 router.put('/update/:id', verifyUser, updateListing);
+router.get('/get-all', getAllListing);
 router.get('/:id',getListing);
+
 
 
 export default router;
