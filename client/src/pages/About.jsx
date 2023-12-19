c6de4e33799ddfc9999b11631612ca3403143f8e
Basim Hilal V <basimhilalofficial@gmail.com>
2023-12-19 08:34:08 +0530

    about page created and added some text

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='p-28 max-w-6xl m-auto flex flex-col gap-3'>
+      <h1 className='text-3xl text-blue-200 font-bold text-center'>About Us</h1>
+      <p className='text-blue-100 text-sm'>
+        basi.play is a virtual platform which sell and rent games and consoles for platforms like playstation, xbox, windows, android, and mac through online.
+        we bring the best experience to all the customers around the world and we appreciate the feedback and reviews from customers. This business idea 
+        is purely coordinated and run by basi.play organization and no other illegal sales or rent happening here.
+      </p><p className='text-blue-100 text-sm'>
+        This shopping website is under the license and hold of basi.play and any inconvenience and problems with the service through online will be montiored and rectified according to customer needs. 
+        We give our full effor to bring the best for customer experience. Here you can enquire and search our products and we deliver as soon as possible.
+      </p>
+      <p className='text-blue-100 text-sm'>
+        Our official branch is located in Kerala, India and you can contact us or send enquiries through basi.play@g.co.in and we respond immediately. 
+        Go shopping everyday and get hitched in this world of gaming
+      </p>
+    </div>
   )
 }
 
