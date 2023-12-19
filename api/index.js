787fef0d8d4f6b0f797a85a82da1b218ea93f39e
Basim Hilal V <basimhilalofficial@gmail.com>
2023-12-19 08:54:06 +0530

    code pre installation for hosting

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.DB_URL).then(()=>{
@@ -13,6 +14,7 @@ mongoose.connect(process.env.DB_URL).then(()=>{
     console.log(err);
 })
 
+const __dirname = path.resolve();
 const app = express();
 
 app.listen(3000, ()=>{
@@ -24,6 +26,11 @@ app.use(cookieParser());
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','index.html'))
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
